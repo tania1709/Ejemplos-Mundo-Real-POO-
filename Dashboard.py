c226aa9f376e595aac06aa02caf046f2866c64cc
Tania Guaman <tg.guamang@uea.edu.ec>
2025-02-02 22:08:24 -0500

    Tania Guamán Explicación En el programa solo he creado 2 carpeta semana 4 y semana 5, con subcarpetas de ejemplos o filas integradas por Semana 06.py y Semana 07py; no hubo mucho que modificar porque esta ordenado

---
+++ Dashboard.py
@@ -0,0 +1,48 @@
+import os
+
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+
+
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+
+    opciones = {
+        '1': 'Semana 4 / Ejemplo 1 / Ejemplo 2 / Ejemplo 3'
+        '2': 'Semana 5 / Ejemplo /  Semana 06.py / Semana 07.py'
+
+
+        # Agrega aquí el resto de las rutas de los scripts
+    }
+
+    while True:
+        print("\n********Menu Principal - Dashboard*************")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+
+
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
